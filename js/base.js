65b018393b094aa133d22b86291ebf9d270938b4
Aditya Rathnam <aditya.rathnam@gmail.com>
2014-03-03 22:44:05 -0800

    Hardcoded centered layouts

---
+++ js/base.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+    if ($('#loading-icon').length) {
+        $('#loading-icon').hide();
+    }
+    document.getElementById('hide-till-load').style.display = 'none';
+}
