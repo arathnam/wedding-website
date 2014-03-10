36976ab3170f13e79f6f3a9b8bb0e99d1e6574af
Aditya Rathnam <aditya.rathnam@gmail.com>
2014-03-10 09:27:27 -0700

    Lower casing group names in the database

--- guest_loader.py
+++ guest_loader.py
@@ -8,7 +8,7 @@ class GuestLoader(bulkloader.Loader):
         bulkloader.Loader.__init__(self, 'Guest',
                                    [('last_name', lambda x: x.decode('utf-8')),
                                     ('first_name', lambda x: x.decode('utf-8')),
-                                    ('group_name', lambda x: x.decode('utf-8')),
+                                    ('group_name', lambda x: x.decode('utf-8').lower()),
                                     ('garba', int),
                                     ('ceremony', int),
                                     ('reception', int),
